0abc3185be4f94f33bac0adaa02473a0dfb75ea4
Aayush Raghuvamshi <aayush.raghuvamshi@gmail.com>
2026-02-17 13:20:18 -0800

    feat: added debug print to see path

--- src/git.cpp
+++ src/git.cpp
@@ -25,7 +25,24 @@ namespace git {
             std::cerr << e.what() << '\n';
         }
     }
-    std::string catfile(std::string filepath) {
-        return "";
+    void catfile(std::string path) {
+        std::cout << "Looking for object with hash: " << path << '\n';
+        std::string filepath = ".git/objects/" + path.substr(0, 2) + "/" + path.substr(2);
+        zstr::ifstream file(filepath);
+        if (!file.is_open()) {
+            std::cerr << "Failed to open file: " << filepath << '\n';
+            return;
+        }
+        std::array<char, 64*1024> buffer;
+        std::string file_content;
+        while(file) {
+            file.read(buffer.data(), buffer.size());
+            std::streamsize n = file.gcount();
+            if (n > 0) {
+                file_content.append(buffer.data(), static_cast<size_t>(n));
+            }
+        }
+        std::string content = file_content.substr(file_content.find('\0') + 1);
+        std::cout << content;
     }
 }
