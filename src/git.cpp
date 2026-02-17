c5a3ae87e944d8c988ad5486a018627383e97269
Aayush Raghuvamshi <aayush.raghuvamshi@gmail.com>
2026-02-17 13:25:07 -0800

    print filepath

--- src/git.cpp
+++ src/git.cpp
@@ -28,6 +28,7 @@ namespace git {
     void catfile(std::string path) {
         std::cout << "Looking for object with hash: " << path << '\n';
         std::string filepath = ".git/objects/" + path.substr(0, 2) + "/" + path.substr(2);
+        std::cout << "Constructed file path: " << filepath << '\n';
         zstr::ifstream file(filepath);
         if (!file.is_open()) {
             std::cerr << "Failed to open file: " << filepath << '\n';
